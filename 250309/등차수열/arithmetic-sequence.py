ec136d1ccacb16903c2644ba43712aa61f2836e5
jjun012 <163534242+jjun012@users.noreply.github.com>
2025-03-09 21:55:36 +0900

    Title: 등차수열, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250309/등차수열/arithmetic-sequence.py
+++ 250309/등차수열/arithmetic-sequence.py
@@ -1,9 +1,4 @@
-# 입력 받기
-N = int(input())  # N개의 수
-arr = list(map(int, input().split()))  # N개의 수
-
-# 수열을 집합(set)으로 변환하여 빠르게 검색할 수 있도록 함
-arr_MAX_A = 100
+MAX_A = 100
 
 # 변수 선언 및 입력
 n = int(input())
@@ -26,13 +21,3 @@ for x in range(1, MAX_A + 1):
     ans = max(ans, cnt)
     
 print(ans)
-set = set(arr)
-
-# 쌍의 개수 세기
-count = 0
-for num in arr:
-    if num + K in arr_set:  # num + K가 수열에 존재하면 그 쌍을 만족함
-        count += 1
-
-# 결과 출력
-print(count)
