4e2b981326729c48b29b7aec4f96d59775b98d5f
jjun012 <163534242+jjun012@users.noreply.github.com>
2025-04-30 16:39:54 +0900

    Title: 배수의 개수 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/배수의 개수 2/number-of-multipliers-2.py
+++ 250430/배수의 개수 2/number-of-multipliers-2.py
@@ -1,6 +1,9 @@
 cnt=0
-for i in range(1,11):
-    
-    if i%2!=0:
+num=[]
+for i in range(10):
+    n=int(input())
+    num.append(n)
+for i in range(len(num)):
+    if num[i]%2!=0:
         cnt+=1
 print(cnt)
